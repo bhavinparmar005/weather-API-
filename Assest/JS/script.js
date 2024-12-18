a5a587e310cb2a8d3da2c5b43f44923a20eb34fa
bhavinparmar005 <bhavinparmar005@gmail.com>
2024-12-17 18:45:32 -0800

    projects

--- Assest/JS/script.js
+++ Assest/JS/script.js
@@ -1,15 +1,6 @@
 let searchBotton = document.getElementById("search_button");
 let apiKey = "094b1cddc40a67c426c17b4f56de2b4d";
 let div =document.querySelector('.container');
-
-
-
-
-
-
-
-
-
 let defaultCity = "Amreli";
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${apiKey}&units=metric`)
@@ -24,13 +15,13 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${
                             `
               );
     div.innerHTML = `
-     <form class="search_main">
+            <form class="search_main" id="search_main">
                 <div class="search_input">
                     <input type="text" placeholder="Search City" id="search_city">
 
                 </div>
                 <div class="search_button">
-                    <button type="submit" id="search_button">
+                    <button id="search_button" onclick="searchData()">
                         <div class="search_icon">
                             <img src="./Assest/ICONS/magnifying-glass-solid.svg" alt="search_icon">
                         </div>
@@ -87,43 +78,108 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${
     `
 })
 
-// searchBotton.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     let userInput = document.getElementById("search_city");
-//   let city = userInput.value.trim();
-//   if (city === "") {
-//     alert("Please enter a city name");
-//   }
+function searchData(){
 
-//   console.log(city);
-
-//   async function getData() {
-//     try {
-//       let response = await fetch(
-//         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-//       );
-
-//       let finalResult = await response.json();
-
-//       console.log(finalResult);
-
-
-//       let div = document.querySelector('.container');
-
+    let form = document.getElementById('search_main');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let userInput = document.getElementById("search_city");
+      let city = userInput.value.trim();
+      if (city === "") {
+        alert("Please enter a city name");
+      }
+      else{
+        async function getData() {
+            try {
+              let response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+              );
+        
+              let finalResult = await response.json();
       
-
-//       console.log(
-//         `
-//                     celsius    :- ${finalResult.main.temp}c
-//                     city       :- ${finalResult.name}
-//                     humidity   :- ${finalResult.main.humidity}%
-//                     wind speed :- ${finalResult.wind.speed}Km/h
-//                     `
-//       );
-//     } catch (error) {
-//       console.log("error");
-//     }
-//   }
-
-//   getData();
-// });
+              div.innerHTML = ""
+    
+              div.innerHTML = `
+              <form class="search_main" id="search_main">
+                  <div class="search_input">
+                      <input type="text" placeholder="Search City" id="search_city">
+    
+                  </div>
+                  <div class="search_button">
+                      <button id="search_button" onclick="searchData()">
+                          <div class="search_icon">
+                              <img src="./Assest/ICONS/magnifying-glass-solid.svg" alt="search_icon">
+                          </div>
+                      </button>
+    
+                  </div>
+    
+              </form>
+    
+              <div class="weather_img_main">
+                  <div class="weather_img">
+                      <img src="./Assest/ICONS/white-sun-with-small-cloud.svg" alt="">
+                  </div>
+    
+              </div>
+    
+              <div class="celsius_display">
+                  <h1>${finalResult.main.temp}<sup>o</sup>c</h1>
+              </div>
+    
+              <div class="city_name">
+                  <p>${finalResult.name}</p>
+    
+              </div>
+    
+              <div class="humidity_and_windSpeed_main">
+                  <div class="humidity">
+                      <div class="humidity_icon">
+                          <img src="./Assest/ICONS/water.svg" alt="">
+                      </div>
+                      <div class="humidity_pre">
+                          <h3>${finalResult.main.humidity}%</h3> 
+                          <p>Humidity</p>
+                      </div>
+    
+                  </div>
+    
+                  <div class="wind_speed_main">
+                      <div class="wind_speed_icon">
+                            <img src="./Assest/ICONS/wind.svg" alt="">
+                      </div>
+    
+                      <div class="wind_speed_text">
+                          <h3>${finalResult.wind.speed} km/h</h3> 
+                          <p>Wind Speed</p>
+                      </div>
+    
+                  </div>
+    
+    
+              </div>
+    
+      
+      `
+console.log(
+                `
+                            celsius    :- ${finalResult.main.temp}c
+                            city       :- ${finalResult.name}
+                            humidity   :- ${finalResult.main.humidity}%
+                            wind speed :- ${finalResult.wind.speed}Km/h
+                            `
+              );
+            } catch (error) {
+              console.log("error");
+            }
+          }
+        
+          getData();
+        
+      }
+    
+      
+    
+    
+    });
+}
